72df16301f5571feecd2b0ace32be8e0ae42b979
Sahil-Odedra <sahilodedra26@gmail.com>
2025-07-20 15:04:02 +0530

    basic error fixed

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from models import db, User, Cameraman, Booking
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.secret_key = 'cameramanbooking123'
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 app.config['images'] = os.path.join(BASE_DIR, 'static', 'images')
@@ -34,9 +35,8 @@ def register_user():
         db.session.add(new_user)
         db.session.commit()
 
-        return "Registration successfull"
-    
-    return render_template('login_user.html')
+        return render_template('login_user.html')
+    return render_template('user_register.html')
 
 
 @app.route('/login_user', methods=['GET', 'POST'])
@@ -53,6 +53,7 @@ def login_user():
             error = "Invalid Credentials"
     return render_template('login_user.html', error=error)
 
+
 @app.route('/cameraman_profile/<mobile>')
 def profile_cameraman(mobile):
     if 'cameraman_mobile' not in session or session['cameraman_mobile'] != mobile:
@@ -63,10 +64,12 @@ def profile_cameraman(mobile):
         return "No Such User found"
     return render_template('profile_cameraman.html', cameraman=cameraman)
 
-@app.route('/logout')
+
+@app.route('/logout', methods=['POST'])
 def logout():
     session.pop('cameraman_mobile', None)
     return redirect(url_for('login_cameraman'))
+
 
 @app.route('/login_cameraman', methods=['GET', 'POST'])
 def login_cameraman():
@@ -84,7 +87,7 @@ def login_cameraman():
 
 @app.route('/')
 def home():
-    return redirect(url_for('login_cameraman'))
+    return redirect(url_for('login_user'))
 
 @app.route('/register_cameraman', methods=['GET', 'POST'])
 def register_cameraman():
